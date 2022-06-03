403368396c371f1081fe7e027758dfd73d29391d
durga <durgakorada96@gmail.com>
2022-06-03 14:31:51 +0530

    add slide2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { GetStartedComponent } from './get-started/get-started.component';
 
 const routes: Routes = [
   {
@@ -10,7 +11,9 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },
+  }, {
+    path:'get-started', component: GetStartedComponent
+  }
 ];
 
 @NgModule({
